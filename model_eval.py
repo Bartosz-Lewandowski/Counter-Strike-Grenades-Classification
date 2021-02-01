ad5aeb13675af69aea230b2f048d4926305559c8
Bartosz-Lewandowski <112126@student.upwr.edu.pl>
2021-02-02 00:02:18 +0100

    Add scirt to evaluate new predictions

---
+++ model_eval.py
@@ -0,0 +1,12 @@
+import pandas as pd 
+from sklearn.metrics import accuracy_score, confusion_matrix, roc_auc_score
+
+
+data = pd.read_csv('./data/test.csv')
+y_pred = data['LABELS']
+y = pd.read_csv('./data/test_labels.csv')
+
+print('\nROC AUC score', roc_auc_score(y,y_pred))
+print('\nAccuracy score:', accuracy_score(y,y_pred))
+print('\nConfusion matrix:')
+print(confusion_matrix(y,y_pred))
